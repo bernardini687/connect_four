32011fe1a8c3852819465acf63d555ede84e6230
Oscar Bernardini <bernardini687@gmail.com>
2019-08-08 16:08:28 +0200

    To-do for Board tests

---
+++ spec/board_spec.rb
@@ -0,0 +1,17 @@
+# Board.new                # => initialize with default_grid (7x6)
+# Board.new grid: 'smthng' # => initialize with something else
+# Board.grid
+
+# check default has 7 things and each of those has 6
+
+# get cell
+# set cell
+# assess the grid
+
+# FOUR_IN_A_ROW
+
+module ConnectFour
+  RSpec.describe Board do
+
+  end
+end
