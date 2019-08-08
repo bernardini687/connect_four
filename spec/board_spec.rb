31ae3f5cde9f560f4d425dc5fc69651090b762b7
Oscar Bernardini <bernardini687@gmail.com>
2019-08-08 16:31:54 +0200

    First tests for Board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,3 @@
-# Board.new                # => initialize with default_grid (7x6)
-# Board.new grid: 'smthng' # => initialize with something else
-# Board.grid
-
-# check default has 7 things and each of those has 6
-
 # get cell
 # set cell
 # assess the grid
@@ -13,5 +7,36 @@
 module ConnectFour
   RSpec.describe Board do
 
+    context '#initialize' do
+      it 'is successfully initialized without arguments' do
+        expect(Board.new).not_to raise_error ArgumentError
+      end
+
+      it 'sets the grid with 6 rows by default' do
+        board = Board.new
+
+        expect(board.grid).to have(6).things
+      end
+
+      it 'creates 7 things in each row by default' do
+        board = Board.new
+
+        board.grid.each do |row|
+          expect(row).to have(7).things
+        end
+      end
+
+      it 'is successfully initialized with a custom grid' do
+        expect(Board.new grid: :foo).not_to raise_error ArgumentError
+      end
+
+      describe '#grid' do
+        it 'returns the grid' do
+          board = Board.new grid: :foo
+
+          expect(board.grid).to eq :foo
+        end
+      end
+    end
   end
 end
