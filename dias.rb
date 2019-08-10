c12129869ac63c09dec46db75b490723ca27d8d7
Oscar Bernardini <bernardini687@gmail.com>
2019-08-10 15:56:04 +0200

    Find sawtooth diagonals

--- dias.rb
+++ dias.rb
@@ -7,27 +7,37 @@ trx = [
   %w[Q R S T U V Z]
 ]
 
-# [0,0]
-# [0,1], [1,0]
-# [0,2], [1,1], [2,0]
-# [0,3], [1,2], [2,1], [3,0]
-# [0,4], [1,3], [2,2], [3,1], [4, 0]
-# [0,5], [1,4], [2,3], [3,2], [4, 1], [5,0]
-# [0,6], [1,5], [2,4], [3,3], [4, 2], [5, 1]
+def sawt_diagonal(matrix, row: 0, col: 0)
+  max_row = matrix.size
+  max_col = matrix[0].size
 
-# [1,6], [2,5], [3,4], [4,3], [5,2]
-# [2,6], [3,5], [4,4], [5,3]
-# [3,6], [4,5]. [5,4]
-# [4,6], [5,5]
-# [5,6]
+  diagonal = []
 
-# max_col = 7 = row.size
-# max_row = 6 = trx.size
+  while row < max_row && col < max_col
+    diagonal << matrix[row][col]
+    row += 1
+    col += 1
+  end
 
-# [r,c]
-# [r + 1, c - 1] until r is max_row or c is 0
+  diagonal
+end
 
-def diagonal_from(matrix, row: 0, col: 0)
+# p sawt_diagonal(trx, col: 6)
+# p sawt_diagonal(trx, col: 5)
+# p sawt_diagonal(trx, col: 4)
+p sawt_diagonal(trx, col: 3)
+p sawt_diagonal(trx, col: 2)
+p sawt_diagonal(trx, col: 1)
+p sawt_diagonal(trx)
+puts
+p sawt_diagonal(trx, row: 1)
+p sawt_diagonal(trx, row: 2)
+# p sawt_diagonal(trx, row: 3)
+# p sawt_diagonal(trx, row: 4)
+# p sawt_diagonal(trx, row: 5)
+puts
+
+def ramp_diagonal(matrix, row: 0, col: 0)
   max_row = matrix.size
   max_col = matrix[0].size
 
@@ -42,16 +52,16 @@ def diagonal_from(matrix, row: 0, col: 0)
   diagonal
 end
 
-# p diagonal_from(trx)
-# p diagonal_from(trx, col: 1)
-# p diagonal_from(trx, col: 2)
-p diagonal_from(trx, col: 3)
-p diagonal_from(trx, col: 4)
-p diagonal_from(trx, col: 5)
-p diagonal_from(trx, col: 6)
+# p ramp_diagonal(trx)
+# p ramp_diagonal(trx, col: 1)
+# p ramp_diagonal(trx, col: 2)
+p ramp_diagonal(trx, col: 3)
+p ramp_diagonal(trx, col: 4)
+p ramp_diagonal(trx, col: 5)
+p ramp_diagonal(trx, col: 6)
 puts
-p diagonal_from(trx, row: 1, col: 6)
-p diagonal_from(trx, row: 2, col: 6)
-# p diagonal_from(trx, row: 3, col: 6)
-# p diagonal_from(trx, row: 4, col: 6)
-# p diagonal_from(trx, row: 5, col: 6)
+p ramp_diagonal(trx, row: 1, col: 6)
+p ramp_diagonal(trx, row: 2, col: 6)
+# p ramp_diagonal(trx, row: 3, col: 6)
+# p ramp_diagonal(trx, row: 4, col: 6)
+# p ramp_diagonal(trx, row: 5, col: 6)
